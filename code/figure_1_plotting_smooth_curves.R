c268dc7c8c60fa479292adef3c90ba37cfbb0205
eric-pedersen <eric.pedersen@wisc.edu>
2016-02-29 14:16:10 -0600

    Switched from "cs" to "cr" smooths for cubic example.
    "cs" smoooths add a small null-space penalty to the model; I wanted to remove this, as explaining it would likely just add extra mental overhead for readers.

--- code/figure_1_plotting_smooth_curves.R
+++ code/figure_1_plotting_smooth_curves.R
@@ -7,50 +7,50 @@ library(stringr)
 k = 6
 
 plotting_data = data.frame(x = seq(0,1,length=100))
-cs_basis = smoothCon(s(x,bs="cs",k=k+1), data=plotting_data,
+cr_basis = smoothCon(s(x,bs="cr",k=k+1), data=plotting_data,
                      knots=NULL,absorb.cons=TRUE)[[1]]
 tp_basis = smoothCon(s(x,bs="tp",k=k+1), data=plotting_data,
                      knots=NULL,absorb.cons=TRUE)[[1]]
 
 #### Extract basis functions #### 
-cs_basis_funcs = as.data.frame(cs_basis$X)
-names(cs_basis_funcs) = paste("F",1:k, sep="")
-cs_basis_funcs$x = plotting_data$x
-cs_basis_funcs$model = "Cubic spline"
+cr_basis_funcr = as.data.frame(cr_basis$X)
+names(cr_basis_funcr) = paste("F",1:k, sep="")
+cr_basis_funcr$x = plotting_data$x
+cr_basis_funcr$model = "Cubic spline"
 
-tp_basis_funcs = as.data.frame(tp_basis$X)
-names(tp_basis_funcs) = paste("F",1:k, sep="")
-tp_basis_funcs$x = plotting_data$x
-tp_basis_funcs$model = "Thin plate spline"
+tp_basis_funcr = as.data.frame(tp_basis$X)
+names(tp_basis_funcr) = paste("F",1:k, sep="")
+tp_basis_funcr$x = plotting_data$x
+tp_basis_funcr$model = "Thin plate spline"
 
-spline_basis_funcs = rbind(cs_basis_funcs,tp_basis_funcs)
-spline_basis_funcs = gather(spline_basis_funcs,func,value, -x,-model )
+spline_basis_funcr = rbind(cr_basis_funcr,tp_basis_funcr)
+spline_basis_funcr = gather(spline_basis_funcr,func,value, -x,-model )
 
 
 
 ##### Extract penalty matrices ####
-cs_basis_P = as.data.frame(cs_basis$S)
-cs_basis_P = cs_basis_P/max(cs_basis_P)
-names(cs_basis_P) = paste("F",1:k, sep="")
-cs_basis_P$basis_y = factor(paste("F",1:k, sep=""), 
+cr_basis_P = as.data.frame(cr_basis$S[[1]])
+cr_basis_P = cr_basis_P/max(cr_basis_P)
+names(cr_basis_P) = paste("F",1:k, sep="")
+cr_basis_P$basis_y = factor(paste("F",1:k, sep=""), 
                             levels= rev(paste("F",1:k, sep="")))
-cs_basis_P$model = "Cubic spline"
+cr_basis_P$model = "Cubic spline"
 
-tp_basis_P = as.data.frame(tp_basis$S)
+tp_basis_P = as.data.frame(tp_basis$S[[1]])
 tp_basis_P = tp_basis_P/max(tp_basis_P)
 names(tp_basis_P) = paste("F",1:k, sep="")
 tp_basis_P$basis_y = factor(paste("F",1:k, sep=""), 
                             levels= rev(paste("F",1:k, sep="")))
 tp_basis_P$model = "Thin plate spline"
 
-spline_basis_penalties = rbind(cs_basis_P ,tp_basis_P )
+spline_basis_penalties = rbind(cr_basis_P ,tp_basis_P )
 spline_basis_penalties = gather(spline_basis_penalties,basis_x,value,
                                 -basis_y,-model )
 
 
 
 
-basis_func_plot = ggplot(aes(x=x,y=value),data=spline_basis_funcs)+
+basis_func_plot = ggplot(aes(x=x,y=value),data=spline_basis_funcr)+
   geom_line()+
   scale_x_continuous(breaks=seq(0,1,length=5),
                      labels=c("0","0.25","0.5","0.75","1"))+
