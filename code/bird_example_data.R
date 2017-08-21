e2a3e1e524a0de3d6eed13903429606f700c3508
Eric Pedersen <eric.pedersen@wisc.edu>
2017-08-21 10:18:02 -0230

    First work on simulated bird migration example

---
+++ code/bird_example_data.R
@@ -0,0 +1,27 @@
+# we will assume birds range from latitude 0 to latitude 60,
+# with birds varying in in the peak times they reach max and min latitude, 
+# and in time they spend migrating vs. at breeding/feeding grounds 
+# modelled as differences in the shape of the migration curve
+
+library(dplyr)
+library(tidyr)
+set_seed = 10
+species_data = data_frame(species = factor(paste("sp", 1:9, sep="")),
+                          phase_shift = rnorm(9,0,4),
+                          curve_shape = rnorm(9,-.25,0.3),
+                          curve_width = 7,
+                          max_abund = rlnorm(9,log(10), 0.5) 
+                          )
+
+bird_move = crossing(latitude = seq(5,60,length=10),
+                     species = species_data$species,
+                     week = seq(4,52, by=6))%>%
+  left_join(species_data)%>%
+  mutate(peak_lat = -cos((week+phase_shift)*(2*pi/52)),
+         peak_lat = peak_lat*abs(peak_lat)^curve_shape,
+         peak_lat = (peak_lat+1)*30,
+         avg_abund = max_abund*dnorm(latitude,peak_lat, curve_width)/dnorm(1,0, curve_width),
+         count = rpois(n(), avg_abund))
+
+write.csv(bird_move,file = "data/bird_move.csv",row.names = F)
+                          
