ad43e83b82b1c1797bdff9f67dbff1e0332aa1dc
Eric Pedersen <eric.pedersen@wisc.edu>
2017-08-28 23:41:40 -0230

    Changed number of basis functions (k) for CO2 examples
    Realized it was inconsistent between models (I was using k=7 for model one and k=6 for the others), and these functional responses are not so nonlinear that they need 6 degrees of freedom. This will also simplify the table in section V

--- code/Sec_5_computational_code.R
+++ code/Sec_5_computational_code.R
@@ -30,14 +30,14 @@ CO2$Plant_uo = factor(CO2$Plant, levels = levels(CO2$Plant), ordered = F)
 CO2_penmat = create_connected_pmat(CO2$Plant_uo)
 bird_penmat = create_connected_pmat(bird_move$species)
 
-comp_resources[1,"time"] = system.time(CO2_mod1 <- gam(log(uptake) ~ s(log(conc),k=7,m=2, bs="tp")+s(Plant_uo, k =12,  bs="re"), 
+comp_resources[1,"time"] = system.time(CO2_mod1 <- gam(log(uptake) ~ s(log(conc),k=5,m=2, bs="tp")+s(Plant_uo, k =12,  bs="re"), 
                                                     data= CO2,method="REML"))[3]
 
 comp_resources[2,"time"] = system.time(bird_mod1 <- gam(count ~ te(week,latitude, bs= c("cc", "tp"), k=c(10,10)), 
                                                      data= bird_move, method="REML", family= poisson))[3]
 
-comp_resources[3,"time"] = system.time(CO2_mod2a <- gam(log(uptake) ~ s(log(conc),k=6,m=2, bs="tp")+
-                                                       s(log(conc), Plant_uo, k =6,  bs="fs",m=1), 
+comp_resources[3,"time"] = system.time(CO2_mod2a <- gam(log(uptake) ~ s(log(conc),k=5,m=2, bs="tp")+
+                                                       s(log(conc), Plant_uo, k=5, bs="fs",m=1), 
                                                      data= CO2,method="REML"))[3]
 
 
@@ -50,7 +50,7 @@ comp_resources[4,"time"] = system.time(bird_mod2a <- gam(count ~ te(week,latitud
 
 
 comp_resources[5,"time"] = system.time(
-  CO2_mod2b <- gam(log(uptake) ~ te(log(conc),Plant_uo, k=c(6,12),m=2, 
+  CO2_mod2b <- gam(log(uptake) ~ te(log(conc),Plant_uo, k=c(5,12),m=2, 
                                     bs=c("tp","mrf"), xt= list(penalty = CO2_penmat)), 
                    data= CO2,method="REML"))[3]
 
@@ -64,8 +64,8 @@ comp_resources[6,"time"] = system.time(
 
 
 comp_resources[7,"time"] = system.time(
-  CO2_mod3 <- gam(log(uptake) ~ s(log(conc),k=6,m=2, bs="tp")+
-                    s(log(conc),by= Plant_uo, k =6,  bs="ts",m=1)+
+  CO2_mod3 <- gam(log(uptake) ~ s(log(conc),k=5,m=2, bs="tp")+
+                    s(log(conc),by= Plant_uo, k =5,  bs="ts",m=1)+
                     s(Plant_uo,bs="re",k=12), 
                   data= CO2,method="REML"))[3]
 
@@ -80,7 +80,7 @@ comp_resources[8,"time"] = system.time(
 
 
 comp_resources[9,"time"] = system.time(
-  CO2_mod4 <- gam(log(uptake) ~ s(log(conc), Plant_uo, k =6,  bs="fs",m=2), 
+  CO2_mod4 <- gam(log(uptake) ~ s(log(conc), Plant_uo, k=5,  bs="fs",m=2), 
                   data= CO2,method="REML"))[3]
 
 
@@ -92,7 +92,7 @@ comp_resources[10,"time"] = system.time(
 
 
 comp_resources[11,"time"] = system.time(
-  CO2_mod5 <- gam(log(uptake) ~ s(log(conc),by= Plant_uo, k =6,  bs="tp",m=2)+
+  CO2_mod5 <- gam(log(uptake) ~ s(log(conc),by= Plant_uo, k =5,  bs="tp",m=2)+
                     s(Plant_uo,bs="re",k=12), data= CO2,method="REML")
   
 )[3]
