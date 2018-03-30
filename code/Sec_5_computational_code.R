5505836e4b5f00cd1f915c163f025de5cefa4e77
Eric Pedersen <eric.pedersen@wisc.edu>
2018-03-30 13:02:29 -0230

    Moved text discussing Markov Random Field smotthers to a separate appendix
    This should simplify the text, as well as speed up the time it takes to build the paper.

--- code/Sec_5_computational_code.R
+++ code/Sec_5_computational_code.R
@@ -4,8 +4,6 @@ library(dplyr)
 library(tidyr)
 source("../code/functions.R")
 
-get_n_coef = function(model) length(coef(model))
-
 get_n_pen  = function(model) {
   family = model$family[[1]]
   if(family %in% c("Gamma","gaussian")){
@@ -16,19 +14,21 @@ get_n_pen  = function(model) {
   return(out_val)
 }
 
+get_n_coef = function(model) length(coef(model))
+
 get_n_iter = function(model) model$outer.info$iter
 get_n_out_iter = function(model) model$iter
   
 bird_move = read.csv("../data/bird_move.csv")
-comp_resources = crossing(model_number = c("1","2a","2b","3","4","5"),
+
+comp_resources = crossing(model_number = c("1","2","3","4","5"),
                        data_source = factor(c("CO2","bird_move"),
                                      levels = c("CO2","bird_move")),
                        time = 0, n_smooths = 0,
                        n_coef = 0)
 
 CO2$Plant_uo = factor(CO2$Plant, levels = levels(CO2$Plant), ordered = F)
-CO2_penmat = create_connected_pmat(CO2$Plant_uo)
-bird_penmat = create_connected_pmat(bird_move$species)
+
 
 comp_resources[1,"time"] = system.time(CO2_mod1 <- gam(log(uptake) ~ s(log(conc),k=5,m=2, bs="tp")+s(Plant_uo, k =12,  bs="re"), 
                                                     data= CO2,method="REML",
@@ -38,13 +38,13 @@ comp_resources[2,"time"] = system.time(bird_mod1 <- gam(count ~ te(week,latitude
                                                      data= bird_move, method="REML", family= poisson,
                                                      control = list(keepData=TRUE)))[3]
 
-comp_resources[3,"time"] = system.time(CO2_mod2a <- gam(log(uptake) ~ s(log(conc),k=5,m=2, bs="tp")+
+comp_resources[3,"time"] = system.time(CO2_mod2 <- gam(log(uptake) ~ s(log(conc),k=5,m=2, bs="tp")+
                                                        s(log(conc), Plant_uo, k=5, bs="fs",m=1), 
                                                      data= CO2,method="REML",
                                                      control = list(keepData=TRUE)))[3]
 
 
-comp_resources[4,"time"] = system.time(bird_mod2a <- gam(count ~ te(week,latitude, bs= c("cc", "tp"), 
+comp_resources[4,"time"] = system.time(bird_mod2 <- gam(count ~ te(week,latitude, bs= c("cc", "tp"), 
                                                                  k=c(10,10),m=c(2,2))+
                                                         te(week,latitude,species, bs= c("cc", "tp","re"), 
                                                            k=c(10,10,6),m = c(1,1,1)), 
@@ -52,24 +52,7 @@ comp_resources[4,"time"] = system.time(bird_mod2a <- gam(count ~ te(week,latitud
                                                       control = list(keepData=TRUE)))[3]
 
 
-
 comp_resources[5,"time"] = system.time(
-  CO2_mod2b <- gam(log(uptake) ~ te(log(conc),Plant_uo, k=c(5,12),m=2, 
-                                    bs=c("tp","mrf"), xt= list(penalty = CO2_penmat)), 
-                   data= CO2,method="REML",
-                   control = list(keepData=TRUE)))[3]
-
-
-comp_resources[6,"time"] = system.time(
-  bird_mod2b <- gam(count ~ te(week,latitude,species, bs= c("cc", "tp","mrf"), 
-                               k=c(10,10,6),m = c(2,2,2),
-                               xt =list(penalty= bird_penmat)), 
-                    data= bird_move, method="REML", family= poisson,
-                    control = list(keepData=TRUE)))[3]
-
-
-
-comp_resources[7,"time"] = system.time(
   CO2_mod3 <- gam(log(uptake) ~ s(log(conc),k=5,m=2, bs="tp")+
                     s(log(conc),by= Plant_uo, k =5,  bs="ts",m=1)+
                     s(Plant_uo,bs="re",k=12), 
@@ -78,7 +61,7 @@ comp_resources[7,"time"] = system.time(
 
 
 
-comp_resources[8,"time"] = system.time(
+comp_resources[6,"time"] = system.time(
   bird_mod3 <- gam(count ~ te(week,latitude, bs= c("cc", "tp"), 
                               k=c(10,10),m=c(2,2)) +
                      te(week,latitude, bs= c("cc", "tp"), 
@@ -87,13 +70,13 @@ comp_resources[8,"time"] = system.time(
                    control = list(keepData=TRUE)))[3]
 
 
-comp_resources[9,"time"] = system.time(
+comp_resources[7,"time"] = system.time(
   CO2_mod4 <- gam(log(uptake) ~ s(log(conc), Plant_uo, k=5,  bs="fs",m=2), 
                   data= CO2,method="REML",
                   control = list(keepData=TRUE)))[3]
 
 
-comp_resources[10,"time"] = system.time(
+comp_resources[8,"time"] = system.time(
   bird_mod4 <- gam(count ~ te(week,latitude,species, bs= c("cc", "tp","re"), 
                               k=c(10,10,6),m = 2), 
                    data= bird_move, method="REML", family= poisson,
@@ -101,14 +84,14 @@ comp_resources[10,"time"] = system.time(
 )[3]
 
 
-comp_resources[11,"time"] = system.time(
+comp_resources[9,"time"] = system.time(
   CO2_mod5 <- gam(log(uptake) ~ s(log(conc),by= Plant_uo, k =5,  bs="tp",m=2)+
                     s(Plant_uo,bs="re",k=12), data= CO2,method="REML",
                   control = list(keepData=TRUE))
   
 )[3]
 
-comp_resources[12,"time"] = system.time(
+comp_resources[10,"time"] = system.time(
   bird_mod5 <- gam(count ~ te(week,latitude,by=species, bs= c("cc", "tp"), 
                               k=c(10,10),m = 2), 
                    data= bird_move, method="REML", family= poisson,
@@ -116,9 +99,9 @@ comp_resources[12,"time"] = system.time(
 )[3]
 
 
-comp_resources$model = list(CO2_mod1, bird_mod1, CO2_mod2a, bird_mod2a, 
-                         CO2_mod2b, bird_mod2b, CO2_mod3, bird_mod3,
-                         CO2_mod4, bird_mod4, CO2_mod5, bird_mod5)
+comp_resources$model = list(CO2_mod1, bird_mod1, CO2_mod2, bird_mod2,
+                            CO2_mod3, bird_mod3,CO2_mod4, bird_mod4, 
+                            CO2_mod5, bird_mod5)
 
 comp_resources = comp_resources %>%
   group_by(model_number, data_source)%>%
