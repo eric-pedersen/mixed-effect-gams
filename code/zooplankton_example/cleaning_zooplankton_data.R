9a86f7f97193de80f78c4c5a8d9bc86b73cd18df
Eric Pedersen <eric.pedersen@wisc.edu>
2018-04-21 19:57:25 -0230

    Fleshed out the Zooplankton dynamics example

--- code/zooplankton_example/cleaning_zooplankton_data.R
+++ code/zooplankton_example/cleaning_zooplankton_data.R
@@ -5,30 +5,43 @@ library(Hmisc)
 
 #data from: https://portal.lternet.edu/nis/mapbrowse?packageid=knb-lter-ntl.262.2
 
+#Density here is a somewhat specialized data type, in that it is based off counts from 
+#subsamples, but then scaled to the full sample and rounded to the nearest thousand.
+#This makes it very difficult to treat it as either a count type or a continous
+#zero-inflated variable. I've chosen to deal with this by replacing any zero
+#value with the minumum observed density, then log-transforming and scaling within 
+#species and year. 
+
 zooplankton_data = read.csv("data/madisonlakeszoopoldnet.csv",stringsAsFactors = F)%>%
-  filter(lakeid == "ME",
-         !taxon  %in% c("DAPHNIA MENDOTAE JUVENILE", 
+  filter(!taxon  %in% c("DAPHNIA MENDOTAE JUVENILE", #excluding juvenile stages
                         "DAPHNIA PULICARIA JUVENILE",
+                        "DAPHNIA RETROCURVA JUVENILE",
+                        "DIAPHANOSOMA BIRGEI JUVENILE",
                         "COPEPOD NAUPLII"))%>%
-  mutate(date = lubridate::as_date(sampledate),
+  mutate(date = lubridate::as_date(sampledate), #adjusting labels
          day  = yday(date),
          taxon = tolower(taxon),
-         taxon = Hmisc::capitalize(taxon)
+         taxon = Hmisc::capitalize(taxon),
+         lake  = case_when(lakeid=="ME"~"Mendota",
+                           lakeid=="MO"~"Menona",
+                           lakeid=="KE"~"Kegonsa",
+                           lakeid=="WA"~"Waubesa")
          )%>%
   rename(year = year4)%>%
-  dplyr::select(taxon, year, day, density, avg_length)%>%
+  dplyr::select(taxon,lake, year, day, density)%>% #filtering out extraneous columns
   group_by(taxon)%>%
-  filter(n()>150)%>%
+  filter(n()>400)%>% #filtering to only keep the most common species
   ungroup()%>%
-  complete(nesting(day, year), 
+  complete(nesting(day, year,lake), 
            taxon, 
-           fill = list(density=NA, avg_length=NA))%>%
-  mutate(present = as.numeric(!is.na(avg_length)))%>%
+           fill = list(density=NA))%>%
+  mutate(present = !is.na(density))%>%
   group_by(taxon)%>%
   mutate(density = ifelse(is.na(density), min(density,na.rm = T),density))%>%
-  group_by(taxon,year)%>%
+  group_by(taxon,year,lake)%>%
   filter(any(present))%>%
   mutate(density_scaled = scale(log10(density)))%>%
-  ungroup()
+  ungroup()%>%
+  select(-present)
 
-write.csv(zooplankton_data, "data/zooplankton_example.csv")
+write.csv(zooplankton_data, "data/zooplankton_example.csv",row.names = FALSE)
